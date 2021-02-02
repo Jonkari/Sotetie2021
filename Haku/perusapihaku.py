3d0fe11b59a69f8dd5cdb1a95d1ca117b338df75
Tommi Puurunen <Tommi.Puurunen@edu.savonia.fi>
2021-02-02 10:49:49 +0200

    Päivitys perusapihakuun

--- Haku/perusapihaku.py
+++ Haku/perusapihaku.py
@@ -1,7 +1,8 @@
 import requests
-
-data = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+import json
+data = requests.get('https://opintopolku.fi/lo/search?start=0&rows=25&lang=fi&searchType=LO&text=avoin%20yo', headers={"Caller-Id":"JokuStringivaa"})
 print(data.json())
-
-print(data.json()['userId'])
+fo=open("data.json", "w")
+fo.write(json.dumps(data.json(), indent=1, sort_keys=True))
+fo.close()
 print(type(data.json()))
