33ad4aadefb431a9eef51f5e5a9b4bfe3ccb70a6
Tommi Puurunen <Tommi.Puurunen@edu.savonia.fi>
2021-02-08 22:35:48 +0200

    API malli, tietokanta luokka, joka toimii FLASKin kanssa

--- api/api.py
+++ api/api.py
@@ -1,1 +1,26 @@
-# api tänne, kuuluu db yhteys yms
+import json
+import time
+import pymysql.cursors
+from tietokanta import Database
+from flask import json
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_caching import Cache
+
+app = Flask(__name__)
+api = Api(app)
+cache = Cache(app, config={
+    'CACHE_TYPE':'simple'
+})
+
+db = Database("localhost", "root", "", "wordpress")
+
+class News(Resource):
+    def __init__(self,):
+        super().__init__()
+    @cache.cached(timeout=60)
+    def get(self,):
+        return json.jsonify(db.getData("SELECT * FROM news ORDER BY timestamp DESC"))
+api.add_resource(News, '/api', '/api/etiikka')
+if __name__ =='__main__':
+    app.run(host="0.0.0.0", debug=True)
--- api/tietokanta.py
+++ api/tietokanta.py
@@ -1,1 +1,76 @@
-#tietokanta yms
+import time
+import pymysql.cursors
+import queue
+class Database:
+    class ConnPool():
+        def __init__(self, size, max_size, host, user, password, db):
+            self.db_args = {
+                "host" : host,
+                "user" : user,
+                "password" : password,
+                "db" : db
+            }
+            self.max_size = max_size
+            self.pool = queue.Queue(max_size)
+            for i in range(size):
+                conn = pymysql.connect(
+                        host=self.db_args["host"],
+                        user=self.db_args["user"],
+                        password=self.db_args["password"],
+                        db=self.db_args["db"]
+                    )
+                conn.autocommit(True)
+                self.pool.put(
+                    conn
+                )
+        def getConnection(self,):
+            try:
+                return self.pool.get_nowait()
+            except queue.Empty:
+                if self.pool.qsize < self.max_size:
+                    conn = pymysql.connect(
+                            host=self.db_args["host"],
+                            user=self.db_args["user"],
+                            password=self.db_args["password"],
+                            db=self.db_args["db"]
+                    )
+                    conn.autocommit(True)
+                    self.pool.put(
+                        conn
+                    )
+                else:
+                    raise Exception("Pool error")
+        def putConnection(self, conn):
+            if conn:
+                conn.cursor().close()
+            try:
+                self.pool.put_nowait(conn)
+            except queue.Full:
+                print("Queue Full")
+    def __init__(self, host, user, password, db):
+        self.connection_pool = self.ConnPool(10, 800, host, user, password, db)
+    def query(self, query, params):
+        conn = self.connection_pool.getConnection()
+        if conn:
+            with conn.cursor(pymysql.cursors.DictCursor) as cursor:
+                if params:
+                    cursor.execute(query.format(**params))
+        self.connection_pool.putConnection(conn)
+    def getData(self, query, params=None):
+        conn = self.connection_pool.getConnection()
+        conn.ping()
+        data = None
+        if conn:
+            with conn.cursor(pymysql.cursors.DictCursor) as cursor:
+                if params:
+                    cursor.execute(query.format(**params))
+                else:
+                    cursor.execute(query)
+                data = cursor.fetchall()
+        self.connection_pool.putConnection(conn)
+        return data
+    def escape(self, var):
+        conn = self.connection_pool.getConnection()
+        data = conn.escape(var)
+        self.connection_pool.putConnection(conn)
+        return data
