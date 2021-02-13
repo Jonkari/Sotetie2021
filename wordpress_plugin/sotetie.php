3d5636b907bd4595a9b663d9ed7a80b8ed929023
Tommi Puurunen <Tommi.Puurunen@edu.savonia.fi>
2021-02-13 12:33:30 +0200

    Tiedostopathit käyttämään WP omaa funktiota, tyylitiedoston liittomahdollisuus

--- wordpress_plugin/sotetie.php
+++ wordpress_plugin/sotetie.php
@@ -1,12 +1,22 @@
 <?php
 /**
- * Plugin Name: Testi
+ * Plugin Name: Sotetie
  */
+
 function sotetie($content) {
     if(is_page('testi')) {
-        $content .= "<table><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr></table>";
+        $content .= file_get_contents(plugins_url('kayttoliittyma.html', __FILE__));
     }
     return $content;
 }
-add_filter( 'the_content', 'sotetie');
+function tiedostoLiitto() {
+    $tyyli_tiedosto = null;
+    if($tyyli_tiedosto != null) {
+        wp_register_style('sotetie_haku', plugins_url($tyyli_tiedosto, __FILE__));
+        wp_enqueue_style('sotetie_haku');
+    }
+}
+
+add_filter('the_content', 'sotetie');
+add_action('wp_enqueue_scripts', 'tiedostoLiitto')
 ?>
