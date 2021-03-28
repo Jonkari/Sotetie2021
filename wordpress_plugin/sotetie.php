9bf7f6c0e0fec092187abe5d110377ad9794bb69
Tommi Puurunen <Tommi.Puurunen@edu.savonia.fi>
2021-03-28 12:39:46 +0300

    Suoraan linkistä haku "haku" muuttujalla
    ?haku=

--- wordpress_plugin/sotetie.php
+++ wordpress_plugin/sotetie.php
@@ -5,20 +5,37 @@
 
 function sotetie($content) {
     if(is_page('testi')) {
+        if ( get_query_var('haku') ) {
+            $content .= "
+            <script>
+            var valmisHaku = function() {document.querySelector(\".aihe\").value = \"".get_query_var('haku')."\"}
+            </script>
+            ";
+            }
         $content .= file_get_contents(plugin_dir_path(__FILE__).'kayttoliittyma.html');
+
     }
+    
     return $content;
 }
+function add_query_vars_filter( $vars ){
+    $vars[] = "haku";
+    return $vars;
+  }
+
 function tiedostoLiitto() {
-    $tyyli_tiedosto = "styles.css";
-    if($tyyli_tiedosto != null) {
-        wp_register_style('sotetie_haku', plugins_url($tyyli_tiedosto, __FILE__));
-        wp_enqueue_style('sotetie_haku');
-        wp_deregister_script('jquery');
-        wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+    if(is_page('testi')) {
+        $tyyli_tiedosto = "styles.css";
+        if($tyyli_tiedosto != null) {
+            wp_register_style('sotetie_haku', plugins_url($tyyli_tiedosto, __FILE__));
+            wp_enqueue_style('sotetie_haku');
+            wp_deregister_script('jquery');
+            wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+        }
     }
 }
 
 add_filter('the_content', 'sotetie');
-add_action('wp_enqueue_scripts', 'tiedostoLiitto')
+add_action('wp_enqueue_scripts', 'tiedostoLiitto');
+add_filter( 'query_vars', 'add_query_vars_filter' );
 ?>
