63956a36179eda51ebfec7da63c73a69a03e2805
Tommi Puurunen <Tommi.Puurunen@edu.savonia.fi>
2021-03-29 15:42:56 +0300

    Kielihaku ruotsiksi, käyttöliittymän svg kuvat pois ja tilalle merkit

--- api/opintopolku/opintopolku.py
+++ api/opintopolku/opintopolku.py
@@ -10,8 +10,9 @@ def haeDataa(hakusanat, facetFilter="et01.05.03",hakutyyppi="ongoing"):
 
     Args:
         hakusanat (str): Hakusanat, millä haetaan.
-        facetFilter (str, optional): Arvot ovat: et01.05.03 (Yliopisto) tai et01.04.03 (AMK)  Defaults to "et01.05.03".
-        hakutyyppi (str, optional): Hakutyyppi, mahdolliset arvot ovat ongoing, upcoming ja upcomingLater. Defaults to "ongoing".
+        kieli (str): Kieli, millä haetaan.
+        facetFilter (str, optional): Arvot ovat: et01.05.03 (Yliopisto) tai et01.04.03 (AMK)  Vakioarvo "et01.05.03".
+        hakutyyppi (str, optional): Hakutyyppi, mahdolliset arvot ovat ongoing, upcoming ja upcomingLater. Vakioarvo "ongoing".
 
 
     Returns:
@@ -48,108 +49,273 @@ def hakuTyokaluYksinkertainen():
     """
     try:
         hakusanat_dict = {
-            "asiakaslähtöisyys": [
-                "asiakaslähtöi",
-                "osallisuus",
-                "kohtaaminen",
-                "palvelutar",
-                "asiakasprosessi"
-            ],
-            "ohjaus- ja neuvontaosaaminen" : [
-                "ohjaus",
-                "neuvonta",
-                "palvelujärjestelmä",
-                "vuorovaikutus",
-                "kommunikaatio"
-            ],
-            "palvelujärjestelmät" : [
-                "palvelujärjestelmät",
-                "palvelujärjestelmä",
-                "palveluohjaus",
-            ],
-            "lainsäädäntö ja etiikka" : [
-                "etiikka",
-                "lainsäädäntö",
-                "tietosuoja",
-                "vastuu",
-                "eettinen"
-            ],
-            "tutkimus- ja kehittämisosaaminen" : [
-                "tutkimus",
-                "innovaatio",
-                "kehittäminen"
-            ],
-            "robotiikka ja digitalisaatio" : [
-                "robotiikka",
-                "digi",
-                "tekoäly",
-                "sote-palvelut",
-                "tietoturva",
-                "tietosuoja",
-            ],
-            "vaikuttavuus- kustannus- ja laatutietoisuus" : [
-                "laatu",
-                "laadun",
-                "vaikuttavuu",
-                "vaikutusten",
-                "kustannukset"
-            ],
-            "kestävän kehityksen osaaminen" : [
-                "kestävä",
-                "ekolog",
-                "kestävyys",
-                "kierrätys",
-                "ympäristö",
-                "energiankulutus"
-            ],
-            "viestintäosaaminen" : [
-                "viestintä",
-                "tunnetila",
-                "empatia",
-                "selkokieli",
-                "selko"
-            ],
-            "työntekijyysosaaminen" : [
-                "osaamisen",
-                "johtaminen",
-                "työhyvinvointi",
-                "muutososaaminen",
-                "muutosjoustavuus",
-                "urakehitys",
-                "verkostotyö",
-                "työyhteisö",
-                "moniammatillisuus"
-            ],
-            "monialainen yhteistoiminta" : [
-                "monialaisuu",
-                "moniammatillisuu",
-                "monitiet",
-                "yhteistyö",
-                "verkostoituminen",
-                "asiantuntijuus"
-            ]
+            "asiakaslähtöisyys": {
+                "fi" : [
+                    "asiakaslähtöi",
+                    "osallisuus",
+                    "kohtaaminen",
+                    "palvelutar",
+                    "asiakasprosessi"
+                ],
+                "se" : [
+                    "delaktighet",
+                    "klientcentrering",
+                    "klientcent",
+                    "klientorient",
+                    "delaktighet",
+                    "bemötande",
+                    "serviceutbud",
+                    "servicebehov",
+                    "klientprocess"
+                ],
+                "en" : [
+
+                ]
+            },
+            "ohjaus- ja neuvontaosaaminen" : {
+                "fi" : [
+                    "ohjaus",
+                    "neuvonta",
+                    "palvelujärjestelmä",
+                    "vuorovaikutus",
+                    "kommunikaatio"
+                ],
+                "se" : [
+                    "handleda",
+                    "handledning",
+                    "handled",
+                    "rådgiva",
+                    "rådgiv",
+                    "servicesystem",
+                    "interaktion",
+                    "växelverkan",
+                    "kommunikation"
+                ],
+                "en" : [
+
+                ]
+            },
+            "palvelujärjestelmät" : {
+                "fi" : [
+                    "palvelujärjestelmät",
+                    "palvelujärjestelmä",
+                    "palveluohjaus"
+                ],
+                "se" : [
+                    "servicesystem",
+                    "servicehandledning",
+                    "servicehandl",
+                    "information",
+                    "rådgivning",
+                    "servicenätverk",
+                    "serviceproducent",
+                ],
+                "en" : [
+
+                ]
+            },
+            "lainsäädäntö ja etiikka" : {
+                "fi" : [
+                    "etiikka",
+                    "lainsäädäntö",
+                    "tietosuoja",
+                    "vastuu",
+                    "eettinen"
+                ],
+                "se" : [
+                    "etik",
+                    "lagstiftning",
+                    "dataskydd",
+                    "datasäkerhet",
+                    "sekretess",
+                    "ansvar",
+                    "etisk",
+                ],
+                "en" : [
+
+                ]
+            },
+            "tutkimus- ja kehittämisosaaminen" : {
+                "fi" : [
+                    "tutkimus",
+                    "innovaatio",
+                    "kehittäminen"
+                ],
+                "se" : [
+                    "forskning",
+                    "innovation",
+                    "utveckling",
+                ],
+                "en" : [
+
+                ]
+            },
+            "robotiikka ja digitalisaatio" : {
+                "fi" : [
+                    "robotiikka",
+                    "digi",
+                    "tekoäly",
+                    "sote-palvelut",
+                    "tietoturva",
+                    "tietosuoja",
+                ],
+                "se" : [
+                    "robotik",
+                    "digitalisering",
+                    "digi",
+                    "articifiell intelligens",
+                    "service inom social- och hälsovård",
+                    "sekretess",
+                    "informationssäkerhet",
+                ],
+                "en" : [
+
+                ]
+            },
+            "vaikuttavuus- kustannus- ja laatutietoisuus" : {
+                "fi" : [
+                    "laatu",
+                    "laadun",
+                    "vaikuttavuu",
+                    "vaikutusten",
+                    "kustannukset"
+                ],
+                "se" : [
+                    "kvalitet",
+                    "kvalit",
+                    "effektivitet",
+                    "effekt",
+                    "kostnader",
+                ],
+                "en" : [
+
+                ]
+            },
+            "kestävän kehityksen osaaminen" : {
+                "fi" : [
+                    "kestävä",
+                    "ekolog",
+                    "kestävyys",
+                    "kierrätys",
+                    "ympäristö",
+                    "energiankulutus"
+                ],
+                "se" : [
+                    "hållbar",
+                    "ekolog",
+                    "hållbarhet",
+                    "sortera",
+                    "miljö",
+                    "omgivning",
+                    "energiförbrukning",
+                ],
+                "en" : [
+
+                ]
+            },
+            "viestintäosaaminen" : {
+                "fi" : [
+                    "viestintä",
+                    "tunnetila",
+                    "empatia",
+                    "selkokieli",
+                    "selko"
+                ],
+                "se" : [
+                    "kommunikation",
+                    "känslotillstånd",
+                    "empati",
+                    "klarspråk",
+                    "klarhet",
+                ],
+                "en" : [
+
+                ]
+            },
+            "työntekijyysosaaminen" : {
+                "fi" : [
+                    "osaamisen",
+                    "johtaminen",
+                    "työhyvinvointi",
+                    "muutososaaminen",
+                    "muutosjoustavuus",
+                    "urakehitys",
+                    "verkostotyö",
+                    "työyhteisö",
+                    "moniammatillisuus"
+                ],
+                "se" : [
+                    "kompetens",
+                    "kunnande",
+                    "kunskaper",
+                    "färdigheter",
+                    "ledarskap",
+                    "arbetsvälmående",
+                    "resiliens",
+                    "karriärutveckling",
+                    "nätverksarbete",
+                    "arbetsgemenskap",
+                    "interprofessionell",
+                    "interprof",
+                    "mångprofessionell",
+                    "mångprof",
+                ],
+                "en" : [
+
+                ]
+            },
+            "monialainen yhteistoiminta" : {
+                "fi" : [
+                    "monialaisuu",
+                    "moniammatillisuu",
+                    "monitiet",
+                    "yhteistyö",
+                    "verkostoituminen",
+                    "asiantuntijuus"
+                ],
+                "se" : [
+                    "mångsektoriell",
+                    "mångvetenskaplig",
+                    "mångdisciplinär",
+                    "tvärsektoriell",
+                    "sektorövergripande",
+                    "yrkesövergripande",
+                    "mångprofessionell",
+                    "tvärvetenskaplig",
+                    "samarbete",
+                    "nätverksarbete",
+                    "nätverksbildning",
+                    "sakkunnig",
+                    "expert",
+                    "specialist",
+                ],
+                "en" : [
+
+                ]
+            },
         }
         for facetFilter in ['et01.05.03', 'et01.04.03']:
             for osaaminen, hakusanat in hakusanat_dict.items():
-                for hakusana in hakusanat:
-                    for i in haeDataa(hakusana, facetFilter):
-                        reg = re.match("(\d+).*", i.get('credits'))
-                        if reg:
-                            kurssi_obj = kurssi.Kurssi(
-                                re.sub(r'Avoin yo[:,\s]+', '', i.get('name')),
-                                reg.group(1),
-                                i.get('lopNames')[0],
-                                i.get('id'),
-                                "",
-                                osaaminen,
-                                "",
-                                ""
-                            )
-                        if i.get('id') not in objs:
-                            objs[i.get('id')] = kurssi_obj
-                        else:
-                            if osaaminen not in objs[i.get('id')].osaamiset:
-                                objs[i.get('id')].osaamiset += "|"+osaaminen
+                for hakusana in hakusanat.values():
+                    for sana in hakusana:
+                        for i in haeDataa(sana, facetFilter):
+                            reg = re.match("(\d+).*", i.get('credits'))
+                            if reg:
+                                kurssi_obj = kurssi.Kurssi(
+                                    re.sub(r'Avoin yo[:,\s]+', '', i.get('name')),
+                                    reg.group(1),
+                                    i.get('lopNames')[0],
+                                    i.get('id'),
+                                    "",
+                                    osaaminen,
+                                    "",
+                                    ""
+                                )
+                            if i.get('id') not in objs:
+                                objs[i.get('id')] = kurssi_obj
+                            else:
+                                if osaaminen not in objs[i.get('id')].osaamiset:
+                                    objs[i.get('id')].osaamiset += "|"+osaaminen
         haeLisatiedot()
         tmp = objs
     except Exception as e:
@@ -159,6 +325,11 @@ def haeLisatiedot():
     for i, j in objs.items():
         data = haeKurssinTiedot(i)
         j.kieli = data.get('teachingLanguages')[0]
+        if data.get('teachingLanguages')[0] in ['svenska']:
+            j.kieli = "ruotsi"
+        elif data.get('teachingLanguages')[0] in ['english']:
+            j.kieli = "englanti"
+        
         j.postinumero = data.get('provider').get('postalAddress').get('postalCode')
 
         opetustyyppi_tmp = data.get("teachingPlaces") + data.get("formOfTeaching")
