3f1cd2c7bb886e2bfff6ff0f8b4c231b9a3dea23
Roosa Turunen <roosa.turunen@gmail.com>
2021-04-07 10:40:45 +0300

    korjaus

--- api/hakutyokalunyksikkotestit.py
+++ api/hakutyokalunyksikkotestit.py
@@ -12,7 +12,7 @@ try:
     testi_tapaus(type(opintopolku.haeKurssinTiedot("1.2.246.562.17.27705333006")), dict, True, 6)
     testi_tapaus(type(opintopolku.haeKurssinTiedot("1.2.246.562.17.33742529283")), dict, True, 7)
     testi_tapaus(type(opintopolku.haeDataa("mitenTääMukaToimii")), list, True, 8)
-    #testi_tapaus(len(opintopolku.haeDataa("mitäTähänPitäsLaittaa") > 0), True,  False, 9)
+    testi_tapaus(len(opintopolku.haeDataa("mitäTähänPitäsLaittaa")) > 0, True,  False, 9)
     testi_tapaus(len(opintopolku.haeDataa("asiakaslähtöisyys")), 0, True, 10)
 
     count = 11
